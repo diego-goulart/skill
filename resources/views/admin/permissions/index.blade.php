9af5e5f54fa73d275e183923f93e96ea15ca6e9c
Diego Goulart <diego@djgoulart.com.br>
2017-01-18 17:04:06 -0200

    Ajustes em roles index

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -1,41 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container">
-        <h2>Permissions</h2>
 
-        <a href="{{route('admin.permissions.create')}}">Create</a>
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3>Permissões</h3>
+                    <a href="{{route('admin.permissions.create')}}"
+                       class="btn btn-sm btn-primary pull-right">
+                        Novo</a>
+                </div>
 
-        <br>
-        <br>
+                <div class="panel-body">
+                    <table class="table table-bordered">
+                        <thead>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Action</th>
+                        </thead>
+                        <tbody>
+                        @foreach($permissions as $permission)
+                            <tr>
+                                <td>{{$permission->name}}</td>
+                                <td>{{$permission->description}}</td>
+                                <td>
 
-        <table class="table table-bordered">
-            <thead>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Action</th>
-            </thead>
-            <tbody>
-            @foreach($permissions as $permission)
-                <tr>
-                    <td>{{$permission->name}}</td>
-                    <td>{{$permission->description}}</td>
-                    <td>
+                                    <a href="{{route('admin.permissions.edit',['id'=>$permission->id])}}"
+                                       class="btn btn-default">
+                                        Edit
+                                    </a>
 
-                        <a href="{{route('admin.permissions.edit',['id'=>$permission->id])}}" class="btn btn-default">
-                            Edit
-                        </a>
+                                    <a href="{{route('admin.permissions.destroy',['id'=>$permission->id])}}"
+                                       class="btn btn-danger">
+                                        Destroy
+                                    </a>
 
-                        <a href="{{route('admin.permissions.destroy',['id'=>$permission->id])}}" class="btn btn-danger">
-                            Destroy
-                        </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{$permissions->links()}}
+                </div>
+            </div>
+        </div>
 
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
     </div>
 
 @endsection
--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -3,51 +3,53 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Roles
-                        <a href="{{route('admin.roles.create')}}"
-                           class="btn btn-sm btn-primary pull-right">
-                            Novo</a>
-                    </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
 
-                    <div class="panel-body">
-                        <table class="table table-bordered">
-                            <thead>
+                </div>
+
+                <div class="panel-body">
+                    <h3 class="pull-left">Roles</h3>
+                    <a href="{{route('admin.roles.create')}}"
+                       class="btn btn-sm btn-primary pull-right">
+                        Novo</a>
+
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th width="30%">Ações</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($roles as $role)
                             <tr>
-                                <th>Nome</th>
-                                <th>Descrição</th>
-                                <th width="30%">Ações</th>
+                                <td>{{$role->name}}</td>
+                                <td>{{$role->description}}</td>
+                                <td>
+                                    {!! link_to_route('admin.roles.edit',
+                                    $title = 'Editar',
+                                    ['id' => $role->id],
+                                    ['class' => 'btn btn-sm btn-default']) !!}
+
+                                    <a href="{{route('admin.roles.permissions',['id'=>$role->id])}}"
+                                       class="btn btn-sm btn-default">
+                                        Permissões
+                                    </a>
+
+                                    {!! link_to_route('admin.roles.destroy',
+                                    $title = 'Excluir',
+                                    ['id' => $role->id],
+                                    ['class' => 'btn btn-sm btn-danger']) !!}
+
+
+                                </td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($roles as $role)
-                                <tr>
-                                    <td>{{$role->name}}</td>
-                                    <td>{{$role->description}}</td>
-                                    <td>
-                                        {!! link_to_route('admin.roles.edit',
-                                        $title = 'Editar',
-                                        ['id' => $role->id],
-                                        ['class' => 'btn btn-link']) !!}
-
-                                        {!! link_to_route('admin.roles.destroy',
-                                        $title = 'Excluir',
-                                        ['id' => $role->id],
-                                        ['class' => 'btn btn-link']) !!}
-
-                                        <a href="{{route('admin.roles.permissions',['id'=>$role->id])}}"
-                                           class="btn btn-link btn-danger">
-                                            Permissões
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        {{$roles->links()}}
-                    </div>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{$roles->links()}}
                 </div>
             </div>
         </div>
