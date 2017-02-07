298d4a080bdb83ca95be696a894e5ef922176530
Diego Goulart <diego@djgoulart.com.br>
2017-02-07 16:43:32 -0200

    Inserção dos novos dados no cabeçalho de visualização das monitorias

--- resources/views/admin/reports/view.blade.php
+++ resources/views/admin/reports/view.blade.php
@@ -7,9 +7,36 @@
                 <div class="panel panel-default">
                     <div class="panel-header">
                         <div class="col col-md-12">
-
-                            <h3>{{$report->operador->name}} - {{$report->created_at->format('d-m-Y H:i')}} - {{number($report->total)}}</h3>
+                            <div class="clearfix"></div>
+                            <br>
                             <a href="{{route('admin.lider')}}" class="btn btn-default pull-right">Voltar</a>
+                            <div class="clearfix"></div>
+                            <br>
+                           <table class="table table-bordered">
+                               <tbody>
+                               <tr>
+                                   <th width="20%">Operador</th>
+                                   <td class="text-uppercase">{{$report->operador->name}}</td>
+                               </tr>
+                               <tr>
+                                   <th width="20%">Data da Monitoria</th>
+                                   <td>{{$report->created_at->format('d/m/Y H:i')}}</td>
+                               </tr>
+                               <tr>
+                                   <th width="20%">Data da ligação</th>
+                                   <td>{{date('d/m/Y H:i', strtotime($report->dt_call .' '. $report->tm_call ))}}</td>
+                               </tr>
+                               <tr>
+                                   <th width="20%">Cliente</th>
+                                   <td class="text-uppercase">{{$report->ctm_name}}</td>
+                               </tr>
+                               <tr>
+                                   <th width="20%">Nota Final</th>
+                                   <td class="text-uppercase">{{$report->total}}</td>
+                               </tr>
+                               </tbody>
+                           </table>
+
                         </div>
                        <div class="clearfix"></div>
                     </div>
@@ -28,3 +55,4 @@
         </div>
     </div>
 @endsection
+-  - {{number($report->total)}}
